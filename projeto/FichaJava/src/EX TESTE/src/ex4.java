b2184a3f15693e6da038869e864673b19b3c7dd1
Thiago Pereira <thiagoh88@gmail.com>
2024-06-19 21:57:53 +0100

    ficha1

---
+++ projeto/FichaJava/src/EX TESTE/src/ex4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double num1,pi=3.14;
+        System.out.println("entrar com raio");
+        num1 = input.nextDouble();
+        num1=2*pi*num1;
+        System.out.println("area:" +num1);
+
+
+
+    }
+}
---
+++ projeto/FichaJava/src/EX TESTE/src/ex5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        double num1,num2,num3,media;
+        System.out.println("introduza 1");
+        num1= input.nextDouble();
+        System.out.println("introduza 2");
+        num2= input.nextDouble();
+        System.out.println("introduza 3");
+        num3= input.nextDouble();
+
+        num1=(num1+num2+num3)/3;
+
+        System.out.println("media: "+num1);
+        media=num1*0.2;
+        System.out.println("20%: "+media);
+
+        media=num1*0.3;
+        System.out.println("30%: "+media);
+
+        media=num1*0.4;
+        System.out.println("40%: "+media);
+
+
+
+    }
+}
